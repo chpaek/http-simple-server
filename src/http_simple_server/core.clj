b875125da83c62247f8d28af254b75b8c600d6e4
chang <chpaek@gmail.com>
2023-06-01 16:26:40 +0900

    hello world

--- src/http_simple_server/core.clj
+++ src/http_simple_server/core.clj
@@ -1,6 +1,26 @@
-(ns http-simple-server.core)
+(ns http-simple-server.core
+   (:use ring.adapter.jetty
+        [ring.middleware.content-type]
+        [ring.middleware.cookies]
+        [ring.middleware.params]
+        [ring.middleware.session]
+        [ring.middleware.session.cookie]
+        [ring.util.response]))
+(require '[ring.adapter.jetty :refer [run-jetty]])
+;; (defn foo
+;;   "I don't do a whole lot."
+;;   [x]
+;;   (println x "Hello, World!"))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn handler [request]
+  { :status 200
+   :headers {"Content-Type" "text/html"}
+   :body "hello world!1"
+  }
+  )
+
+(defn -main
+  []  
+  (run-jetty handler {:port 3001}))
+
